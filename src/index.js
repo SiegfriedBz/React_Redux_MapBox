5219c8c68f6990c863c6192c4b4cf9b6ec78f823
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-24 16:52:29 +0200

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from './App';
 import "../src/assets/application.scss"
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { logger } from 'redux-logger'
 import flatsReducer from "./reducers/flats_reducer"
 import SelectFlatReducer from "./reducers/selectFlat_reducer"
 
-import { logger } from 'redux-logger'
 
 const middleWares = applyMiddleware(logger)
 
