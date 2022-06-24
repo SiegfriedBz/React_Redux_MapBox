00bcc4bb2ae15c0f5f323e425b385e2170783adf
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-24 20:20:54 +0200

    removed redux-logger, added redux-devtool + redux-promise

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../src/assets/application.scss"
 import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { logger } from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import promiseMiddleware from 'redux-promise';
 import flatsReducer from "./reducers/flats_reducer"
 import SelectFlatReducer from "./reducers/selectFlat_reducer"
-
-
-const middleWares = applyMiddleware(logger)
 
 const reducers = combineReducers({
   flats: flatsReducer,
   selectedFlat: SelectFlatReducer
 })
 
-const store = createStore(reducers, {}, middleWares)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleWares = applyMiddleware(promiseMiddleware)
+
+const store = createStore(reducers, {}, composeEnhancers(middleWares))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
