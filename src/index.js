88bb5e66cd537dff2522bf1910bcbbdbfc185aaf
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-24 16:40:33 +0200

    added redux-logger middleware

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../src/assets/application.scss"
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import flatsReducer from "./reducers/flats_reducer"
 import SelectFlatReducer from "./reducers/selectFlat_reducer"
+
+import { logger } from 'redux-logger'
+
+const middleWares = applyMiddleware(logger)
 
 const reducers = combineReducers({
   flats: flatsReducer,
   selectedFlat: SelectFlatReducer
 })
 
-const store = createStore(reducers)
+const store = createStore(reducers, {}, middleWares)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
